efadeb605924851b44837360181de5191848d043
spicj <spicer847@gmail.com>
2025-12-02 16:30:31 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime, timedelta
+
+df = pd.read_excel("C:\Users\spicj\Documents\OB Lineouts\OBLineouts.xlsx")
